72c0b873fe683b58e58d3704268966e56dfb4969
Redensarten <jan.steffes@gmx.de>
2024-12-05 11:54:56 +0100

    Add Solutions for day 5

---
+++ aoc-2024-unittests/Solution01Tests.cs
@@ -0,0 +1,26 @@
+﻿using aoc_2024.Classes;
+using aoc_2024.Solutions;
+
+namespace aoc_2024_unittests
+{
+    [TestFixture]
+    public class Solution01Tests
+    {
+        [Test]
+        public void TestA()
+        {
+            //arrange
+            var testCase = new TestManager(new TestLogger()).Parse(1).First(test => test.TestNumber == 1);
+            var expected = testCase.AnswerA;
+            var input = testCase.Input;
+            var solution1 = new Solution01();
+
+            // act
+            var result = solution1.RunPartA(input);
+
+            // assert
+            Assert.That(result, Is.EqualTo(expected));
+
+        }
+    }
+}
---
+++ aoc-2024-unittests/TestLogger.cs
@@ -0,0 +1,14 @@
+﻿using aoc_2024;
+using aoc_2024.Interfaces;
+using System.Diagnostics;
+
+namespace aoc_2024_unittests
+{
+    internal class TestLogger : ILogger
+    {
+        public void Log(string message, LogSeverity logSeverity)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
