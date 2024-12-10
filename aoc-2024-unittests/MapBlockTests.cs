e8672b4c16a70e1a73fba7bf86d9fb6e6f6b80e7
Redensarten <jan.steffes@gmx.de>
2024-12-10 19:42:13 +0100

    Overengineered 9B (not working correctly)

---
+++ aoc-2024-unittests/MapBlockTests.cs
@@ -0,0 +1,25 @@
+﻿using aoc_2024.Solutions;
+
+namespace aoc_2024_unittests
+{
+    internal class MapBlockTests
+    {
+
+        [TestCase(1, 5, 1, 5)]
+        [TestCase(1, 5, 2, 15)]
+        [TestCase(2, 5, 2, 25)]
+        [TestCase(10, 5, 5, 300)]
+        public void TestSumCalc(int index, int value, int range, int expectedResult)
+        {
+            // arrange
+            var mapBlock = new MapBlock(value, range);
+
+            // act
+            var result = mapBlock.CalculateValue(index);
+
+            // assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+    }
+}
